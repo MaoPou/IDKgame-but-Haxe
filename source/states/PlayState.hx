79e086ebc63bf2c20691c5c425bfcbaa127cfdaf
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-03-02 20:34:28 +0800

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -5,7 +5,6 @@ import flixel.text.FlxText;
 
 import hscript.Interp;
 import sys.FileSystem;
-import sys.io.File;
 
 class PlayState extends FlxState {
     public var traced:String = 'none';
@@ -20,15 +19,34 @@ class PlayState extends FlxState {
         add(helloText);
         
         var scriptPath = "/storage/emulated/0/gongxiang/FNF-NovaFlare-Engine/scripts/script.hx";
+        var libraryPath = "/storage/emulated/0/gongxiang/FNF-NovaFlare-Engine/scripts/library.txt";
         
-        // 检查脚本文件是否存在
-        if (FileSystem.exists(scriptPath)) {
+        if (FileSystem.exists(scriptPath)
             // 读取文件内容
             var scriptContent = Std.string(File.getContent(scriptPath));
             var parser = new hscript.Parser();
             var ast = parser.parseString(scriptContent);
             var interp = new hscript.Interp();
             traced = interp.execute(ast);
+        
+            if (FileSystem.exists(libraryPath)) {
+                var libraryAll = File.getContent(scriptPath);
+                var lines = libraryAll.split("\n");
+
+                for (line in lines) {
+                    line = line.trim();
+                
+                    var parts = line.split(",");
+                
+                    if (parts.length >= 2) {
+                        var part1 = parts[0];
+                        var part2 = parts.slice(1).join(",");
+                        interp.variables.set(part1 + '.' + part2);
+                    } else {
+                        interp.variables.set(part1);
+                    }
+                }
+            }
         }
     }
 
