1b097506f21d74a87635ee1d6741b2bbdd65f18a
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-03-02 14:29:01 +0800

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -3,16 +3,30 @@ package states;
 import flixel.FlxState;
 import flixel.text.FlxText;
 
+import hscript.Interpreter;
+import sys.io.File;
+import sys.FileSystem;
+
 class PlayState extends FlxState {
     override public function create():Void {
         super.create();
 
-        // 创建一个文本对象
-        var helloText = new FlxText(100, 100, 200, "Hello");
-        helloText.size = 32; // 设置字体大小
-        helloText.color = 0xFFFFFF; // 设置字体颜色为白色
+        var directory = "/storage/emulated/0/Download/";
 
-        // 添加文本到场景中
-        add(helloText);
+        var files = FileSystem.readDirectory(directory);
+
+        for (file in files) {
+            if (file.endsWith(".hx")) {
+                var filePath = directory + file;
+                trace("Loading script: " + filePath);
+
+                // 读取文件内容
+                var scriptContent = File.getContent(filePath);
+
+                // 创建解释器并执行脚本
+                var interpreter = new Interpreter();
+                interpreter.run(scriptContent);
+            }
+        }
     }
 }
