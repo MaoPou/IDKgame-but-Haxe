41543763f5ab728b06a5b3a96baa1246dfa3c2dd
MaoPou <166735337+MaoPou@users.noreply.github.com>
2025-03-02 20:59:44 +0800

    Update PlayState.hx

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -12,42 +12,45 @@ class PlayState extends FlxState {
     override public function create():Void {
         super.create();
         
-        helloText.size = 22; // 设置字体大小
-        helloText.color = 0xFFFFFF; // 设置字体颜色为白色
+        helloText.size = 22;
+        helloText.color = 0xFFFFFF;
 
-        // 添加文本到场景中
         add(helloText);
         
         var scriptPath = "/storage/emulated/0/gongxiang/FNF-NovaFlare-Engine/scripts/script.hx";
         var libraryPath = "/storage/emulated/0/gongxiang/FNF-NovaFlare-Engine/scripts/library.txt";
         
-        if (FileSystem.exists(scriptPath){
-            // 读取文件内容
-            var scriptContent = Std.string(File.getContent(scriptPath));
+        if (FileSystem.exists(scriptPath)){
+            
             var parser = new hscript.Parser();
-            var ast = parser.parseString(scriptContent);
-            var interp = new hscript.Interp();
-            traced = interp.execute(ast);
         
-            if (FileSystem.exists(libraryPath)) {
-                var libraryAll = File.getContent(scriptPath);
-                var lines = libraryAll.split("\n");
+            var ast = parser.parseString(scripts);
+            var interp = new hscript.Interp();
+            
+            var scripts = Std.string(File.getContent(scriptPath));
 
-                for (line in lines) {
-                    line = line.trim();
+            traced = interp.execute(ast);
+        }
+        
+        if (FileSystem.exists(libraryPath)) {
+            var libraryAll = File.getContent(libraryPath);
+            var lines = libraryAll.split("\n");
+
+            for (line in lines) {
+                line = line.trim();
                 
-                    var parts = line.split(",");
+                var parts = line.split(",");
                 
-                    if (parts.length >= 2) {
-                        var part1 = parts[0];
-                        var part2 = parts.slice(1).join(",");
-                        interp.variables.set(part1 + '.' + part2);
-                    } else {
-                        interp.variables.set(part1);
-                    }
+                if (parts.length >= 2) {
+                    var part1 = parts[0];
+                    var part2 = parts.slice(1).join(",");
+                    interp.variables.set(part1 + '.' + part2);
+                } else {
+                    interp.variables.set(part1);
                 }
             }
         }
+        
     }
 
     public function new() {
